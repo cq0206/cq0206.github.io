3883965a349e2bf61102e20507cbc48cd63b6fa1
cq0206 <cq20110310@gmail.com>
2023-10-29 21:10:47 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '...',
+    description: '...',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '这就是编程', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                    { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+                ]
+            }
+        ]
+    }
+}
